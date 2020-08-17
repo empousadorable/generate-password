c7e3a62cc003f5b2a0f63c4212a752414ddafa85
Chelsea Thurman <babefan200@gmail.com>
2020-08-16 23:58:16 -0600

    generator

--- Develop/script.js
+++ Develop/script.js
@@ -9,9 +9,45 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
+
   passwordText.value = password;
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+alert('You chose ' + length);
+
+//ask for character type
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase, or all.");
+alert('You chose ' + charType);
+
+//generate password
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "all" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  }
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
